7dde69894aaa47ff3a956b506ffa0cf65742244c
mohammadzaman1 <mdashikuzzaman17@gmail.com>
2024-08-18 23:54:41 -0400

    mobile fixed

--- app/page.js
+++ app/page.js
@@ -99,7 +99,7 @@ export default function Home() {
   return (
     <Box
       width="100vw"
-      height="100vh"
+      height="100dvh"
       display="flex"
       flexDirection="column"
       justifyContent="center"
